722b452213177fb47e88c0c675589de7c71d2e9a
0rC0 <orco@paranoici.org>
2021-09-12 20:28:05 +0200

    opt_format

--- wmhpypes/interfaces/cat12.py
+++ wmhpypes/interfaces/cat12.py
@@ -10,6 +10,9 @@ from nipype.interfaces.spm.base import (
     SPMCommandInputSpec,
     ImageFileSPM,
 )
+
+from nipype.utils.filemanip import split_filename
+
 from nipype.interfaces.base import (
     InputMultiPath,
     TraitedSpec,
@@ -32,23 +35,79 @@ class CAT12SANLMDenoisingInputSpec(SPMCommandInputSpec):
     in_files = InputMultiPath(
         ImageFileSPM(exists=True),
         field="data",
-        desc="file to segment",
+        desc="Images for filtering.",
         mandatory=True,
         copyfile=False,
     )
 
-    intlim = traits.Int(
-                        100,
-                        usedefault=True,
+    spm_type = traits.Enum(
+        16,
+        0,
+        2,
+        512,
+        field='spm_type',
+        usedefault=True,
+        desc='Data type of the output images. 0 = same, 2 = uint8, 512 = uint16, 16 = single (32 bit)'
+
     )
 
-    addnoise = traits.Float(0.5,
-                            usedefault=True,
-                            desc='strength of additional noise in noise-free regions')
+    intlim = traits.Int(
+        field='intlim',
+        default_value=100,
+        usedefault=True,
+    )
 
-    rician = traits.Int(0,
-                         usedefault=True,
-                         desc='use rician noise distribution')
+    filename_prefix = traits.Str(
+        field='prefix',
+        default_value='sanlm_',
+        usedefault=True,
+        desc='Filename prefix. Specify  the  string  to be prepended to the filenames of the filtered image file(s). Default prefix is "samlm_".',
+    )
+
+    filename_suffix= traits.Str(
+        field='suffix',
+        default_value='',
+        usedefault=True,
+        desc='Filename suffix. Specify  the  string  to  be  appended  to the filenames of the filtered image file(s). Default suffix is "".'
+    )
+
+    addnoise = traits.Float(default_value=0.5,
+                            usedefault=True,
+                            field='addnoise',
+                            desc='Strength of additional noise in noise-free regions. Add  minimal  amount  of noise in regions without any noise to avoid image segmentation problems. This parameter defines the strength of additional noise as percentage of the average signal intensity.')
+
+    rician = traits.Enum(
+        0,
+        1,
+        field='rician',
+        usedefault=True,
+        desc='''Rician noise
+        MRIs  can  have  Gaussian  or  Rician  distributed  noise with uniform or nonuniform variance across the image. If SNR is high enough
+        (>3)  noise  can  be  well  approximated by Gaussian noise in the foreground. However, for SENSE reconstruction or DTI data a Rician
+        distribution is expected. Please note that the Rician noise estimation is sensitive for large signals in the neighbourhood and can lead to
+        artefacts, e.g. cortex can be affected by very high values in the scalp or in blood vessels.''')
+
+    replaceNANandINF = traits.Enum(
+        1,
+        0,
+        field='replaceNANandINF',
+        usedefault=True,
+        desc='Replace NAN by 0, -INF by the minimum and INF by the maximum of the image.'
+    )
+
+    NCstr = traits.Enum(
+        '-Inf',
+        2,
+        4,
+        field='nlmfilter.optimized.NCstr',
+        usedefault=True,
+        desc='''Strength of Noise Corrections
+        Strength  of  the  (sub-resolution)  spatial  adaptive    non local means (SANLM) noise correction. Please note that the filter strength is
+        automatically  estimated.  Change this parameter only for specific conditions. The "light" option applies half of the filter strength of the
+        adaptive  "medium"  cases,  whereas  the  "strong"  option  uses  the  full  filter  strength,  force sub-resolution filtering and applies an
+        additional  iteration.  Sub-resolution  filtering  is  only  used  in  case  of  high image resolution below 0.8 mm or in case of the "strong"
+        option. light = 2, medium = -Inf, strong = 4'''
+    )
 
 
 class CAT12SANLMDenoisingOutputSpec(TraitedSpec):
@@ -58,13 +117,22 @@ class CAT12SANLMDenoisingOutputSpec(TraitedSpec):
 
 class CAT12SANLMDenoising(SPMCommand):
     """
-    Example:
-    =======
-    from wmhpypes.interfaces import cat12
-    c = cat12.CAT12SANLMDenoising()
-    c.inputs.in_files='sub-test_FLAIR.nii'
-    c.inputs.rician = 0
-    c.run()
+    Spatially adaptive non-local means (SANLM) denoising filter
+
+    This  function  applies  an spatial adaptive (sub-resolution) non-local means denoising filter
+    to  the  data.  This  filter  will  remove  noise  while  preserving  edges. The filter strength is
+    automatically estimated based on the standard deviation of the noise.
+
+    This   filter   is  internally  used  in  the  segmentation  procedure  anyway.  Thus,  it  is  not
+    necessary (and not recommended) to apply the filter before segmentation.
+
+
+    Examples
+    --------
+    >>> from nipype.interfaces import cat12
+    >>> c = cat12.CAT12SANLMDenoising()
+    >>> c.inputs.in_files='sub-test_FLAIR.nii'
+    >>> c.run()
     """
 
     input_spec = CAT12SANLMDenoisingInputSpec
@@ -85,15 +153,19 @@ class CAT12SANLMDenoising(SPMCommand):
                 return scans_for_fnames(val)
             else:
                 return scans_for_fname(val)
-        elif opt in ["intlim", "addnoise", "rician"]:
-            return Cell2Str(val)
 
-        return super(CAT12SANLMDenoising2, self)._format_arg(opt, spec, val)
+        return super(CAT12SANLMDenoising, self)._format_arg(opt, spec, val)
 
     def _list_outputs(self):
         outputs = self._outputs().get()
-        outputs['out_file'] = os.path.join(os.getcwd(), 'sanlm_' + self.inputs.in_files[0].split('/')[-1])
+        pth, base, ext = split_filename(self.inputs.in_files[0])
+        outputs['out_file'] = os.path.join(os.getcwd(), self.inputs.filename_prefix +
+                                           base +
+                                           self.inputs.filename_suffix +
+                                           ext)
         return outputs
+
+
 
 class Cell2Str(Cell):
     def __str__(self):
